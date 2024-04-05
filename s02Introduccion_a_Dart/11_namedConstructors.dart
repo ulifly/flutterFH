a48a2e5c4813e56f52123a328808ce87f541ab5c
Ulises Desentis <ulifly@hotmail.com>
2024-04-05 12:24:13 -0600

    s02 cl11

--- s02Introduccion_a_Dart/11_namedConstructors.dart
+++ s02Introduccion_a_Dart/11_namedConstructors.dart
@@ -1,3 +1,41 @@
 void main(){
+  final spiderman = Heroe(
+    nombreHeroe: 'Spiderman',
+    nombre: 'Peter Parker',
+    poder: 'poderes aracnidos',
+    isAlive: true
 
+  );
+
+  final wolverine = Heroe(
+    nombreHeroe: 'Wolverine',
+    nombre: 'Logan',
+    poder: 'Regeneracion',
+    isAlive: false
+  );
+
+
+
+  print(spiderman);
+  print(wolverine);
+}
+
+class Heroe {
+  String nombre;
+  String nombreHeroe;
+  String poder;
+  bool isAlive;
+
+  Heroe ({
+    this.nombre = 'desconocido',
+    required this.nombreHeroe,
+    this.poder = 'sin poder',
+    required this.isAlive
+
+  });
+
+  @override
+  String toString() {
+    return '$nombreHeroe - $poder - nombre real $nombre estado ${ isAlive ? 'disponible':'deceso'}';
+    }
 }
